8166d18b509152c0f8a135ca57042e988ff78a23
Brandon Guzman <studzyog@gmail.com>
2021-03-05 23:53:39 -0600

    Fixed Microsoft Auth cuz I never get things right the first time

--- app/assets/js/microsoft.js
+++ app/assets/js/microsoft.js
@@ -2,7 +2,7 @@
 const request = require('request')
 
 // Constants
-const clientId = 'client id here'
+const clientId = 'bd8ec46b-7e4d-405a-a0b2-ef057d1810bb'
 
 const tokenUri = 'https://login.microsoftonline.com/consumers/oauth2/v2.0/token'
 const authXBLUri = 'https://user.auth.xboxlive.com/user/authenticate'
@@ -78,7 +78,7 @@ function getXSTSToken(XBLToken) {
 
                     case 2148916238: 
                         reject({
-                            message: 'Since you are not yet 18 years old, an adult must add you to a family in order for you to use Helios Launcher!'
+                            message: 'Since you are not yet 18 years old, an adult must add you to a family in order for you to use Studz Launcher!'
                         })
                         return
 
